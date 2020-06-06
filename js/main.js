c65afe8e60c4ab5e2f625b4043a7d351e6f33afd
Zoha-Arif <Zoha-Arif@users.noreply.github.com>
2020-06-06 14:04:29 -0400

    yay it changes functions!

--- js/main.js
+++ js/main.js
@@ -9,30 +9,28 @@ xMax = 10;
 yMin = -10; 
 yMax = 10; 
 
+// n is the number of line segments. 
+var n = 100;
+//i is used inside of the for loop. 
+var i;
+var xPixel, yPixel;   
+var percentX; 
+var percentY;
+
+//these are the math coordinates. 
+var mathX, mathY;
+
+var math = mathjs(),
+    expr = 'sin(x)*(x-1)',
+    scope = {x: 0}; 
+
+function evaluateMathExpr(mathX){
+    scope.x = mathX;
+    var mathY = tree.eval();
+    return mathY; 
+}
+
 function drawCurve(){
-    // n is the number of line segments. 
-    var n = 100;
-    //i is used inside of the for loop. 
-    var i;
-    var xPixel, yPixel;   
-    var percentX; 
-    var percentY;
-
-    //these are the math coordinates. 
-    var mathX, mathY;
-    
-    var math = mathjs(),
-        expr = 'sin(x)*(x-1)',
-        scope = {x: 0},
-        tree = math.parse(expr, scope);
-
-    
-    function evaluateMathExpr(mathX){
-        scope.x = mathX;
-        var mathY = tree.eval();
-        return mathY; 
-    }
-
     c.beginPath();
     for (i = 0; i < n; i++){
         percentX = i / (n - 1);
@@ -49,4 +47,20 @@ function drawCurve(){
     c.stroke();
 }
 
-drawCurve(); 
+function initTextField(){
+    var input = $('#inputField');
+
+    // Set the initial text value programmatically using jQuery.
+    input.val(expr);
+    
+    // Listen for changes using jQuery.
+    input.keyup(function () {
+      expr = input.val();
+      tree = math.parse(expr, scope);
+
+      c.clearRect(0, 0, canvas.width, canvas.height); 
+      drawCurve(); 
+    });
+}
+
+initTextField(); 

e30ef2f030df5d4bd9b7d93e396d457e8d837b68
Zoha-Arif <Zoha-Arif@users.noreply.github.com>
2020-06-13 19:53:19 -0400

    made sum progress  on derivatives function

--- js/simpleDerivatives.js
+++ js/simpleDerivatives.js
@@ -1,4 +1,4 @@
-var samplePoly = "3x^2 + 2x - 4"
+var samplePoly = "3x^2 - 2x - 4 + 5x - 3 + 4x^2 + 3x^3 + 7x - 9x"
 
 var termArrayPlus = []; 
 var termArrayMinus = []; 
@@ -13,12 +13,57 @@ function polyDerivative(samplePoly){
         }
     }
 
+    console.log("CHARRAY INITIAL:");
+    console.log(charArray);
+    var termArrayExponents = []; 
+
+    for (var a = 0; a < charArray.length; a++){
+        console.log("AAAAAA");
+        console.log(a);
+        if (charArray[a] == "^"){
+            console.log("MADE IT!");
+            var afterIndex = a + 1; 
+            termArrayExponents.push(charArray[afterIndex]);
+            charArray.splice(afterIndex, 1);
+            termArrayExponents.push(charArray[a]);
+            charArray.splice(a, 1);
+            var j = a;
+            var horse = 0; 
+            while (horse != 101029383833838383736638373663773){
+                j = j - 1; 
+                if ((charArray[j] == "+") || (charArray[j] == "-") || (typeof charArray[j] == 'undefined')) {
+                    if ((charArray[j] == "+") || (charArray[j] == "-")) {
+                        termArrayExponents.push(charArray[j]);
+                        charArray.splice(j, 1);
+                    } else if (typeof charArray[j] == 'undefined') {
+                        console.log(termArrayExponents);
+                        console.log("RUNNING!");
+                        termArrayExponents.push("+");
+                        console.log(termArrayExponents);
+                        /* 
+                        var beforeIndex1 = termArrayExponents[termArrayExponents.length]; 
+                        if (termArrayExponents[beforeIndex == ]) */
+                    }
+                    horse = 101029383833838383736638373663773;
+                } else {
+                    termArrayExponents.push(charArray[j]);
+                    charArray.splice(j, 1);
+                }
+            }
+        }
+    }
+        termArrayExponents = termArrayExponents.reverse();
+        /*
+        console.log(termArrayExponents);
+        console.log("CHARARRAY"); 
+        console.log(charArray);*/
+
     for (var w = 0; w < charArray.length; w++){
         if (w != 0){
             var beforeIndex = w - 1; 
             if ((charArray[beforeIndex] == "+")){
                 for (var s = w; s != 101029383833838383736638373663773; s++){
-                    if ((charArray[s] == "+") || (charArray[s] == "-")) {
+                    if ((charArray[s] == "+") || (charArray[s] == "-") || (typeof charArray[s] == 'undefined')) {
                         s = 101029383833838383736638373663773;
                     } else {
                         termArrayPlus.push(charArray[s]);
@@ -27,35 +72,32 @@ function polyDerivative(samplePoly){
             }
 
             if ((charArray[beforeIndex] == "-")){
-                termArrayMinus.push(charArray[w]);
                 for (var y = w; y != 101029383833838383736638373663773; y++){
-                    if ((charArray[y] = "+") || (charArray[y] = "-")) {
+                    if ((charArray[y] == "+") || (charArray[y] == "-") || (typeof charArray[y] == 'undefined')) {
                         y = 101029383833838383736638373663773;
                     } else {
-                        termArrayMinus.push(charArray[y])
+                        termArrayMinus.push(charArray[y]);
                     }
                 }
             }
         }
 
         if (w == 0){
-            termArrayPlus.push(charArray[w]);
             for (var x = w; x != 101029383833838383736638373663773; x++){
-                if ((charArray[x] = "+") || (charArray[x] = "-")) {
+                if ((charArray[x] == "+") || (charArray[x] == "-") || (typeof charArray[x] == 'undefined')) {
                     x = 101029383833838383736638373663773;
                 } else {
-                    termArrayPlus.push(charArray[x])
+                    termArrayPlus.push(charArray[x]);
                 }
             }
         }
-        console.log(termArrayPlus);
-        console.log(termArrayMinus);
     }   
+    console.log("THE FINAL PRODUCT:");
+    console.log(termArrayExponents);
+    console.log(termArrayMinus); 
+    console.log(termArrayPlus);
 
     //split up exponential and non-exponential functions.
-    for (var a = 0; a < termArrayPlus.length; a++){
-
-    }
 
     //split up non-exponential and exponential functions but multiply everything by -1 at the end.
 }
